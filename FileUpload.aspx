1d50c44aa3b32ce241510fa45df20bf191703f78
krishthumar6506-oss <krishthumar6506@gmail.com>
2026-08-03 11:06:18 +0530

    FileUpload

---
+++ FileUpload.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FileUpload.aspx.cs" Inherits="Krish_ASP.net.FileUpload" %>
+
+<!DOCTYPE html>
+	<html xmlns="http://www.w3.org/1999/xhtml"><head runat="server">
+ 	   <title>File Upload</title></head>
+	<body>
+  	  <form id="form1" runat="server">
+
+        <h2>File Upload Example</h2>
+
+        <asp:FileUpload
+            ID="FileUpload1"
+            runat="server" />
+
+        <br /><br />
+
+        <asp:Button
+            ID="Upload"
+            runat="server"
+            Text="Upload"
+            OnClick="upload_btn_click" />
+
+        <br /><br />
+
+        <asp:Label
+            ID="lblMessage"
+            runat="server">
+        </asp:Label>
+
+   	 </form></body></html>
---
+++ FileUpload.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Krish_ASP.net
+{
+    public partial class FileUpload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void upload_btn_click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string folderPath = Server.MapPath("~/Uploads/");
+
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                string fileName = Path.GetFileName(FileUpload1.FileName);
+
+                string filePath = Path.Combine(folderPath, fileName);
+
+                FileUpload1.SaveAs(filePath);
+
+                lblMessage.Text = "File uploaded successfully: " + fileName;
+            }
+            else
+            {
+                lblMessage.Text = "Please select a file.";
+            }
+        }
+    }
+}
